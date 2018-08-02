28d6875685324ea3f8a73f7efe711a2680452e60
Twoody <tanner.l.woody@gmail.com>
2018-08-02 17:55:01 -0500

    Init for gradle; Added MainActivity.java which will just be a listview and have a floating button;

---
+++ app/src/main/java/com/example/tanner/taskapp/MainActivty.java
@@ -0,0 +1,11 @@
+package com.example.tanner.taskapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivty extends AppCompatActivity{
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
