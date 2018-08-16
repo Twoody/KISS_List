82eb103d49a66614e2e39ab1d9cb1b29aad18705
Twoody <tanner.l.woody@gmail.com>
2018-08-16 11:30:18 -0500

    Mods for `taskapp` to `KISS_List` package name update;

--- app/src/androidTest/java/com/example/tanner/KISS_List/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/tanner/KISS_List/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.tanner.taskapp", appContext.getPackageName());
+        assertEquals("com.example.tanner.KISS_List", appContext.getPackageName());
     }
 }
