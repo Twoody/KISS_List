3be7bd4285f108a535343cb7bfc764cf96638ac0
Twoody <tanner.l.woody@gmail.com>
2018-09-03 16:55:20 -0500

    First working example with general layout and recyclerview;
    AMC.java and AMC2.java have been swapped;
    Will be removing ACM2.java once full functionality if back for ACM.java;
    AMC2.xml will replace AMC.xml once working conditions apply;

--- app/src/main/java/com/beWoody/tanner/KISS_List/RVCatAdapter.java
+++ app/src/main/java/com/beWoody/tanner/KISS_List/RVCatAdapter.java
@@ -10,9 +10,8 @@ import android.widget.Button;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RVCatAdapter extends RecyclerView.Adapter<RVCatAdapter.ViewHolder>{
+public class RVCatAdapter extends RecyclerView.Adapter<RVCatAdapter.MyViewHolder>{
     private LayoutInflater mInflater;
-    private ItemClickListener mClickListener;
     private List<Categories> catList = new ArrayList<Categories>();
 
     // data is passed into the constructor
@@ -21,57 +20,35 @@ public class RVCatAdapter extends RecyclerView.Adapter<RVCatAdapter.ViewHolder>{
         this.catList = objects;
     }
 
-    // inflates the row layout from xml when needed
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.recyclerview_categories, parent, false);
-        return new ViewHolder(view);
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public Button cat;
+
+        public MyViewHolder(View view) {
+            super(view);
+            cat = (Button) view.findViewById(R.id.RVcatButton);
+        }
     }
 
-    // binds the data to the TextView in each row
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        Categories current = catList.get(position);
-        String cat = current.getCategory();
-        holder.myButton.setText(cat);
+    public RVCatAdapter(List<Categories> catList) {
+        this.catList = catList;
     }
 
-    // total number of rows
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_categories, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Categories category = catList.get(position);
+        holder.cat.setText(category.getCategory());
+    }
+
     @Override
     public int getItemCount() {
         return catList.size();
     }
-
-
-    // stores and recycles views as they are scrolled off screen
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        Button myButton;
-
-        ViewHolder(View itemView) {
-            super(itemView);
-            myButton = itemView.findViewById(R.id.RVcatButton);
-            itemView.setOnClickListener(this);
-        }
-
-        @Override
-        public void onClick(View view) {
-            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-        }
-    }
-
-    // convenience method for getting data at click position
-    Categories getItem(int id) {
-        return catList.get(id);
-    }
-
-    // allows clicks events to be caught
-    void setClickListener(ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }
-
-    // parent activity will implement this method to respond to click events
-    public interface ItemClickListener {
-        void onItemClick(View view, int position);
-    }
-
 }
--- app/src/main/java/com/beWoody/tanner/KISS_List/activity_manage_category.java
+++ app/src/main/java/com/beWoody/tanner/KISS_List/activity_manage_category.java
@@ -2,9 +2,12 @@ package com.beWoody.tanner.KISS_List;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -20,7 +23,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class activity_manage_category extends AppCompatActivity {
+public class activity_manage_category extends AppCompatActivity{
     /*
      *  Author:  Tanner Woody
      *  Date:    20180816
@@ -33,24 +36,26 @@ public class activity_manage_category extends AppCompatActivity {
      *                  characters;
      *       3. Test renaming of lists
      */
+    RVCatAdapter adapter;
     protected TaskerDBHelper db;
-    CatAdapter adapt;
     List<Categories> list2;
     private FloatingActionButton fab;
-    ListView listTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_category);
+        setContentView(R.layout.activity_manage_category2);
 
+        // data to populate the RecyclerView with
         db       = new TaskerDBHelper(this);
         list2    = db.getAllCategories();
-        adapt    = new CatAdapter(this, R.layout.list_categories, list2);
-        listTask = findViewById(R.id.listView_categories);
-        listTask.setAdapter(adapt);
 
-        registerForContextMenu(listTask);
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvCategories);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new RVCatAdapter(this, list2);
+        //adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
         fab = findViewById(R.id.fab_add_category);
         fab.setOnClickListener(new View.OnClickListener(){
@@ -59,156 +64,28 @@ public class activity_manage_category extends AppCompatActivity {
                 startActivity(new Intent(activity_manage_category.this, Pop.class));
             }
         });
-    }//end onCreate()
+    }
 
     @Override
     public void onResume(){
         super.onResume();
         refreshUIThread();
     }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-
-        if(v.getId() == R.id.listView_categories) {
-            ListView catView = (ListView) v;
-            AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
-        }
-        else{
-            Toast.makeText(this, "Tough shootin', Tex.", Toast.LENGTH_LONG).show();
-        }
-        getMenuInflater().inflate(R.menu.category_menu, menu);
-    }//end onCreateContextMenu()
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        int item_id   = item.getItemId();
-        ContextMenu.ContextMenuInfo CMI = item.getMenuInfo();
-        AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) CMI;
-        Categories obj   = adapt.getItem(acmi.position);
-        String category  = obj.getCategory();
-        String catId     = Integer.toString(obj.getId());
-        db               = new TaskerDBHelper(this);
-        boolean ret      = true;
-        String toastText = "";
-        if (item_id == R.id.select_cat)
-            toastText += "Selected " + category;
-        else if(item_id == R.id.delete_cat){
-            Boolean didDelete = db.deleteCat(category);
-            if (didDelete)
-                toastText += "Deleted " + category;
-            else
-                toastText += "\nERROR: DID NOT DELETE `" + category + ";\n\tCATEGORY COULD NOT BE FOUND";
-        }
-        else if(item_id == R.id.rename_cat){
-                //Open a popup window;
-                //autofill editTest with exiting `content`
-                //update table with new `content`
-                //make toast
-            Intent renamePopup = new Intent(activity_manage_category.this, Pop_renameContent_cat.class);
-            renamePopup.putExtra("catId", catId);
-            startActivity(renamePopup);
-            toastText += "Edited task";
-        }
-        else{
-            toastText += "ERROR: NOTHING SELECTED";
-            ret = super.onContextItemSelected(item);
-        }
-        Toast.makeText( this, toastText, Toast.LENGTH_SHORT).show();
-        refreshUIThread();//BUG: NEED A BOOLEAN CHECK IF DATA WAS CHANGED OR NOT
-        return ret;
-    }//end onContextItemSelected
-
     public void refreshUIThread(){
         /*
-        *  Author: Tanner - 20180806
-        *  Data does not dynamically check the data when it is altered;
-        *  Functions to update UI if data is altered;
-        */
+         *  Author: Tanner - 20180806
+         *  Data does not dynamically check the data when it is altered;
+         *  Functions to update UI if data is altered;
+         */
         list2.clear();
         list2.addAll(db.getAllCategories());
-        adapt.notifyDataSetChanged();
-        listTask.invalidateViews();
-        listTask.refreshDrawableState();
+        for (int i=0; i<list2.size(); i++){
+            Categories foo = list2.get(i);
+            Log.d("MEAT22", foo.getCategory());
+        }
+        adapter.notifyDataSetChanged();
+
     }//end refreshUIThread
+}
 
-    public void openMainActivity(){
-        Intent intent = new Intent(this, activity_manage_category.class);
-        startActivity(intent);
-    }//end openMainActivity()
 
-    private class CatAdapter extends ArrayAdapter<Categories> {
-        Context context;
-        List<Categories> catList = new ArrayList<Categories>();
-        int layoutResourceId;
-        private CatAdapter(Context context,
-                          int layoutResourceId,
-                          List<Categories> objects)
-        {
-            super(context, layoutResourceId, objects);
-            this.layoutResourceId = layoutResourceId;
-            this.catList = objects;
-            this.context = context;
-        }//end constructor
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) {
-            Button catbut = null;
-            if (convertView == null) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(
-                        Context.LAYOUT_INFLATER_SERVICE
-                );
-                convertView = inflater.inflate(
-                        R.layout.list_categories,
-                        parent,
-                        false
-                );
-                catbut = convertView.findViewById(
-                        R.id.catButton
-                );
-                convertView.setTag(catbut);
-                catbut.setOnClickListener( new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        Button but                = (Button) v;
-                        Categories changeActivity = (Categories) but.getTag();
-                        String catId              = Integer.toString(changeActivity.getId());
-                        String changeCategory     = changeActivity.getCategory();
-                        Toast toast = Toast.makeText(
-                                getApplicationContext(),
-                                "Opening " + changeCategory,
-                                Toast.LENGTH_LONG
-                        );
-                        toast.setGravity(Gravity.CENTER, 0, 0);
-                        toast.show();
-                        openTasksActivity(catId); //ListView activity of each task per category selected.
-                    }//end onClick()
-                });//end chk.setOnClickListener()
-            }
-            else{
-                catbut = (Button) convertView.getTag();
-            }
-            Categories current    = catList.get(position);
-            String display        = current.getCategory();
-            List listOfTasks      = db.getAllTasks(current.getCategory());
-            List completedTasks   = db.getAllCompletedTasks(current.getCategory());
-            int numberOfTasks     = listOfTasks.size();
-            int numberOfCompleted = completedTasks.size();
-            display += " (" + Integer.toString(numberOfCompleted);
-            display += "/";
-            display += Integer.toString(numberOfTasks) + ")";
-            catbut.setText(display);
-            catbut.setTag(current);
-            return convertView;
-        }//end getView
-
-        public void openTasksActivity(String catId){
-            Intent intent;
-            Class foo = activity_manage_tasks.class;
-            intent    = new Intent(activity_manage_category.this, foo);
-            intent.putExtra("catId", catId);
-            startActivity(intent);
-        }//end openTasksActivity
-    }//end MyAdaper
-}//end activity_manage_category
