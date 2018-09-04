b2dc61fa050e77bfa2b6ae778860ce3fc2888120
Twoody <tanner.l.woody@gmail.com>
2018-09-03 21:47:49 -0500

    Necessary interface file for broken recyclerviews;

---
+++ app/src/main/java/com/beWoody/tanner/KISS_List/ClickListener.java
@@ -0,0 +1,7 @@
+package com.beWoody.tanner.KISS_List;
+
+public interface ClickListener {
+    void onPositionClicked(int position);
+    void onLongClicked(int position);
+    void OnCreateContextMenuListener(int position);
+}
