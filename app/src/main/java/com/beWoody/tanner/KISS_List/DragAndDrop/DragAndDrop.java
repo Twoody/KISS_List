355f7571cdcc8843ba759fa0d302b326b3a8b159
twoody <tanner.l.woody@gmail.com>
2018-09-13 09:02:11 -0700

    DragAndDrop.java updated to working initial state;
    No longer options for what to reorder, just start ordering;
    Swiping for removal still exists;
    Everything moved into one adapter class and one activity;

--- app/src/main/java/com/beWoody/tanner/KISS_List/DragAndDrop/DragAndDrop.java
+++ app/src/main/java/com/beWoody/tanner/KISS_List/DragAndDrop/DragAndDrop.java
@@ -1,13 +1,25 @@
 package com.beWoody.tanner.KISS_List.DragAndDrop;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
+import com.beWoody.tanner.KISS_List.DragHelper.OnStartDragListener;
+import com.beWoody.tanner.KISS_List.DragHelper.SimpleItemTouchHelperCallback;
 import com.beWoody.tanner.KISS_List.R;
 
-public class DragAndDrop extends AppCompatActivity implements MainFragment.OnListItemClickListener {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DragAndDrop extends AppCompatActivity implements OnStartDragListener {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private ItemTouchHelper mItemTouchHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,30 +28,40 @@ public class DragAndDrop extends AppCompatActivity implements MainFragment.OnLis
         setContentView(R.layout.drag_and_drop);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar_drag_and_drop));
 
-        if (savedInstanceState == null) {
-        MainFragment fragment = new MainFragment();
-        getSupportFragmentManager().beginTransaction()
-        .add(R.id.content_drag_and_drop, fragment)
-        .commit();
-        }
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        final String[] items = getResources().getStringArray(R.array.main_items);
+        List<String> mItems = new ArrayList<>();
+        mItems.addAll(Arrays.asList(getApplicationContext().getResources().getStringArray(R.array.dummy_items)));
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        MyAdapter mAdapter = new MyAdapter(getApplicationContext(), this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+
     }
 
     @Override
-    public void onListItemClick(int position) {
-        Fragment fragment = null;
-        switch (position) {
-            case 0:
-            fragment = new RecyclerListFragment();
-            break;
-
-        case 1:
-            fragment = new RecyclerGridFragment();
-            break;
-        }
-
-        getSupportFragmentManager().beginTransaction()
-            .replace(R.id.content_drag_and_drop, fragment)
-            .addToBackStack(null)
-            .commit();
+    public void onResume(){
+        super.onResume();
     }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+    }
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+
 }
